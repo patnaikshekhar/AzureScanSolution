54d7ab468602be460caad88ee999f031af5ddab4
Shekhar Patnaik <shpatnai@microsoft.com>
2018-10-19 16:10:06 +0100

    Added CA-Certs to Dockerfile. Added ability to download the file to the downloads folder

--- scan_service/build.ps1
+++ scan_service/build.ps1
@@ -1,3 +1,5 @@
+$env:GOOS = "linux"
+
 echo "Building App"
 go build
 
@@ -8,4 +10,4 @@ echo "Building container"
 docker build -t patnaikshekhar/clamavrest .
 
 echo "Running container"
-docker run -p 8080:8000 --name av patnaikshekhar/clamavrest
+docker run -p 8080:8000 -e AZ_ACC_NAME=$AZ_ACC_NAME -e AZ_ACC_KEY=$AZ_ACC_KEY --name av patnaikshekhar/clamavrest
--- scan_service/main.go
+++ scan_service/main.go
@@ -1,18 +1,94 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"net/url"
+	"os"
+	"path"
+	"strings"
+
+	"github.com/Azure/azure-storage-blob-go/2018-03-28/azblob"
+)
+
+const (
+	downloadDirectory       = "./downloads"
+	quarantineContainerName = "quarantine"
+	cleanContainerName      = "clean"
 )
 
 func main() {
+
+	accountName := os.Getenv("AZ_ACC_NAME")
+	accountKey := os.Getenv("AZ_ACC_KEY")
+
+	// Create Downloads Directory if does not exists
+	if _, err := os.Stat(downloadDirectory); os.IsNotExist(err) {
+		os.Mkdir(downloadDirectory, os.ModePerm)
+	}
+
 	http.HandleFunc("/scan/", func(w http.ResponseWriter, r *http.Request) {
-		url := r.URL.Path
-		log.Print(url)
-		fmt.Fprintf(w, "This is a response %v", url)
+
+		// Split url and get parts
+		scanurl := r.URL.Path
+		parts := strings.Split(scanurl, "/")
+
+		if len(parts) > 2 {
+
+			fileName := parts[2]
+			log.Printf("Request to download file %s", fileName)
+
+			ctx := context.Background()
+
+			// Download Blob from Container
+			c, err := azblob.NewSharedKeyCredential(accountName, accountKey)
+			if err != nil {
+				sendError(w, err.Error())
+			}
+
+			p := azblob.NewPipeline(c, azblob.PipelineOptions{})
+
+			u, err := url.Parse(fmt.Sprintf("https://%s.blob.core.windows.net", accountName))
+			if err != nil {
+				sendError(w, err.Error())
+			}
+
+			service := azblob.NewServiceURL(*u, p)
+			quarantineContainer := service.NewContainerURL(quarantineContainerName)
+			b := quarantineContainer.NewBlobURL(fileName)
+
+			f, err := os.Create(path.Join("downloads", fileName))
+			if err != nil {
+				sendError(w, err.Error())
+			}
+			defer f.Close()
+
+			// Write Blob to File
+			log.Printf("Downloading Blob %s from url %s", fileName, u)
+			err = azblob.DownloadBlobToFile(ctx, b, 0, 0, f, azblob.DownloadFromBlobOptions{})
+			if err != nil {
+				sendError(w, err.Error())
+			}
+
+			// Scan File
+
+			// Publish result to new container
+
+			// Send Response
+			fmt.Fprint(w, "Done")
+		} else {
+			// Send an Error back
+			sendError(w, "You need to pass an object name")
+		}
 	})
 
 	log.Print("Server is starting")
 	log.Fatal(http.ListenAndServe(":8000", nil))
+}
+
+func sendError(w http.ResponseWriter, err string) {
+	log.Printf("ERROR: %s", err)
+	http.Error(w, err, http.StatusInternalServerError)
 }
