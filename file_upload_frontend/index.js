c687cd4eb4bd2d21ca0066bd7df142944127071a
Shekhar Patnaik <patnaikshekhar@gmail.com>
2018-10-28 21:12:20 +0000

    Improved UI

--- file_upload_frontend/index.js
+++ file_upload_frontend/index.js
@@ -3,6 +3,8 @@ const azure = require('azure-storage')
 const app = express()
 const path = require('path')
 const QUARANTINE_CONTAINER = 'quarantine'
+const CLEAN_CONTAINER = 'clean'
+const VIRUS_CONTAINER = 'virus'
 
 var multer  = require('multer')
 var upload = multer({ dest: 'uploads/' })
@@ -10,6 +12,24 @@ var upload = multer({ dest: 'uploads/' })
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'))
 })
+
+app.get('/fileStatus', async (req, res) => {
+    try {
+        var blobService = azure.createBlobService()
+        res.json({
+            status: 'success',
+            quarantine: await getBlobsForContainer(blobService, QUARANTINE_CONTAINER),
+            clean: await getBlobsForContainer(blobService, CLEAN_CONTAINER),
+            virus: await getBlobsForContainer(blobService, VIRUS_CONTAINER)
+        })          
+    } catch(e) {
+        console.error(e)
+        res.json({
+            status: 'error'
+        })
+    }
+})
+
 
 app.post('/upload', upload.single('file'), (req, res) => {
     var blobService = azure.createBlobService()
@@ -20,9 +40,7 @@ app.post('/upload', upload.single('file'), (req, res) => {
                 error: error
             })
         } else {
-            res.json({
-                status: 'success'
-            })
+            res.redirect('/')
         }
     })
 })
@@ -31,3 +49,15 @@ app.listen(process.env.PORT || 80, () => {
     console.log(`AZURE_STORAGE_ACCOUNT=${process.env.AZURE_STORAGE_ACCOUNT} AZURE_STORAGE_ACCESS_KEY=${process.env.AZURE_STORAGE_ACCESS_KEY}`)
     console.log('Server listening')
 })
+
+function getBlobsForContainer(blobService, containerName) {
+    return new Promise((resolve, reject) => {
+        blobService.listBlobsSegmented(containerName, null, (err, result) => {
+            if (err) {
+                reject(err)
+            } else {
+                resolve(result.entries)
+            }
+        })
+    })
+}
